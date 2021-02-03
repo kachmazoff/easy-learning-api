c10096ddeb16ca6ec9c3b75a09e1dd42e9161904
kachmazoff <alek.kachmazov@yandex.ru>
2021-02-03 13:37:13 +0400

    Deleted unnecessary files

--- src/models/ICard.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEntity } from "./BaseEntity";
-
-export interface ICard extends BaseEntity {
-  title: string;
-  description: string;
-  author_id: string;
-}
--- src/models/ICardsCollectionInfo.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEntity } from "./BaseEntity";
-
-export interface ICardsCollectionInfo extends BaseEntity {
-  title: string;
-  description: string;
-  author_id: string;
-}
