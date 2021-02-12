0ca71ee36f1fa75528c9f46b3cc7da9d6049948d
kachmazoff <alek.kachmazov@yandex.ru>
2021-02-12 13:46:40 +0400

    [START REFACTORING] Create QuestionUtilsService

--- src/services/QuestionsService.ts
+++ src/services/QuestionsService.ts
@@ -2,26 +2,30 @@ import { IQuestion, IQuestionExtended } from "../models";
 import { IQuestionsRepository } from "src/repositories/QuestionsRepository";
 import { ICreateQuestionDTO } from "src/dto/Question";
 import { IAnswersService } from "./AnswersService";
+import { IQuestionUtils, QuestionUtilsService } from "./QuestionUtilsService";
 
 export interface IQuestionsService {
   getAll(): Promise<IQuestionExtended[]>;
   getById(id: string): Promise<IQuestion | void>;
   add(dto: ICreateQuestionDTO): Promise<void>;
   search(queryString: string): Promise<IQuestion[]>;
-  enrich(question: IQuestion): Promise<IQuestionExtended>;
   getUnansweredQuestions(): Promise<IQuestion[]>;
 }
 
 export class QuestionsService implements IQuestionsService {
+  questionUtils: IQuestionUtils;
+
   constructor(
     private readonly questionsRepo: IQuestionsRepository,
     private readonly answersService: IAnswersService
-  ) {}
+  ) {
+    this.questionUtils = new QuestionUtilsService(this.answersService);
+  }
 
   async getAll(): Promise<IQuestionExtended[]> {
     const questions = await this.questionsRepo.getAll();
     const enrichedAnswers = await Promise.all(
-      questions.map((x) => this.enrich(x))
+      questions.map((x) => this.questionUtils.enrich(x))
     );
 
     return enrichedAnswers;
@@ -37,17 +41,6 @@ export class QuestionsService implements IQuestionsService {
 
   async search(queryString: string): Promise<IQuestion[]> {
     return this.questionsRepo.search(queryString);
-  }
-
-  async enrich(question: IQuestion): Promise<IQuestionExtended> {
-    const answersCount = await this.answersService.getAnswersCountForQuestion(
-      question.id
-    );
-
-    return {
-      ...question,
-      answers_count: answersCount,
-    };
   }
 
   getUnansweredQuestions(): Promise<IQuestion[]> {
