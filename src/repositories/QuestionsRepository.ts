a614a555f8148e5f5ef772126fed7b8cb1512e2f
kachmazoff <alek.kachmazov@yandex.ru>
2021-02-12 15:06:42 +0400

    Add getCollectionQuestions to IQuestionsRepository

--- src/repositories/QuestionsRepository.ts
+++ src/repositories/QuestionsRepository.ts
@@ -11,10 +11,12 @@ export interface IQuestionsRepository {
   add(dto: ICreateQuestionDTO): Promise<void>;
   search(queryString: string): Promise<IQuestion[]>;
   getUnansweredQuestions(): Promise<IQuestion[]>;
+  getCollectionQuestions(collectionId: string): Promise<IQuestion[]>;
 }
 
 export class QuestionsRepository implements IQuestionsRepository {
   TABLE_NAME = "QUESTIONS";
+  COLLECTION_QUESTION = "COLLECTION_QUESTION";
 
   async getAll() {
     const res = await query<IQuestion[]>(
@@ -47,6 +49,21 @@ export class QuestionsRepository implements IQuestionsRepository {
 
   async getUnansweredQuestions() {
     const sqlQuery = `SELECT * FROM \`${this.TABLE_NAME}\` WHERE id NOT IN (SELECT DISTINCT question_id FROM \`ANSWERS\`) ORDER BY created DESC`;
+    const res = await query<IQuestion[]>(sqlQuery);
+    return !!res ? res : [];
+  }
+
+  async getCollectionQuestions(collectionId: string) {
+    const sqlQuery = `
+    SELECT q.id AS id,
+       q.data AS data,
+       q.author_id AS author_id
+    FROM
+        (SELECT question_id
+        FROM \`${this.COLLECTION_QUESTION}\`
+        WHERE collection_id='${collectionId}' ORDER BY created DESC) rel
+    JOIN \`${this.TABLE_NAME}\` q ON rel.question_id=q.id
+    `;
     const res = await query<IQuestion[]>(sqlQuery);
     return !!res ? res : [];
   }
